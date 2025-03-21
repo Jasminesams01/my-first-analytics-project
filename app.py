90ca64a0bf7ce0a5b219b47d2e32e577d2a2b3c5
Jasminesams01 <j.sams@tbs-education.org>
2025-03-21 09:18:59 +0000

    yryy

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.title("My first data application")
+st.write("This is a content to start with")
+
+st.title("User CSV File Viewer")
+
+# Request the user to Upload the File 
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+
+if uploaded_file:
+     df = pd.read_csv(uploaded_file)
+     st.write("Here is the preview of the Uploaded CSV: ")
+     st.dataframe(df.head())
+
+     # Chart 1: Bar chart of customers by country
+     st.subheader("Customers by City")
+     country_counts = df['customer_city'].value_counts()
+     st.write(country_counts)
+     st.bar_chart(country_counts)
+
+     # Chart 2: Line chart of subscriptions over time
+     st.subheader("Trips Over Time")
+     df['Trip Date'] = pd.to_datetime(df['pickup_time']).dt.date
+     subscriptions_by_date = df.groupby('Trip Date').size().reset_index(name='Count')
+     st.line_chart(subscriptions_by_date.set_index('Trip Date'))
